71b6f0dfbcab8a63c6c53d4c35d8ef15f3b735fb
fernsky <trilochanbhusal000@gmail.com>
2025-06-27 22:45:51 +0545

    fix: rename familyData to rawfamilyData in sample-data.ts for clarity

--- src/modules/odk/buddhashanti-services/parser/family/sample-data.ts
+++ src/modules/odk/buddhashanti-services/parser/family/sample-data.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const familyData = {
+const rawfamilyData = {
   intro: null,
   audio_monitoring: '1737550865719.m4a',
   enumerator_introduction: {
